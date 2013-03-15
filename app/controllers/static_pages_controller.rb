5c770ceda3bfea094a6bf0d91e76aa34f5ece700
Bishoy <marcustadros036@gmail.com>
2013-03-15 13:21:07 -0400

    working pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,21 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def submit
+    if params[:guess] == 'Author'
+      flash[:success] = "Correct! Claim your free gift below."  
+      redirect_to answer_path 
+    else
+      flash[:alert] = "You didn't get it, but we'll give you something anyway. See below."
+      redirect_to answer_path 
+    end
+  end
+
+  def hint
+  end
+
+  def answer
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Waltz::Application.routes.draw do
 
   match '/guess_my_job',    to: 'static_pages#home', :as => "home"
+  post '/submit',    to: 'static_pages#submit', :as => "submit"
+  match '/hint',    to: 'static_pages#hint', :as => "hint"
+  match '/answer',    to: 'static_pages#answer', :as => "answer"
 end
